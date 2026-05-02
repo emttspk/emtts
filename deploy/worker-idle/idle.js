33715f5e98a835b9d1e54fc572330b175d912b47
Nazim Saeed <nazimsaeed@gmail.com>
2026-05-02 18:28:39 +0500

    fix: silence worker idle heartbeat

--- deploy/worker-idle/idle.js
+++ deploy/worker-idle/idle.js
@@ -1,4 +1,2 @@
 console.log("[Worker] Worker service disabled. Queue processing is handled by the API container.");
-setInterval(() => {
-  console.log("[Worker] idle heartbeat");
-}, 60_000);
+setInterval(() => undefined, 60_000);
